31729b964988790c32816814c75b51dd0559b24a
dyfire <dyfire@gmail.com>
2017-02-27 17:28:34 +0800

    Update Pool.php

--- Pool.php
+++ Pool.php
@@ -1,54 +1,66 @@
 <?php
 
-namespace Pm; 
+namespace Pm;
 
 /**
  * process pool
  *
  */
-class Pool {
+class Pool
+{
     private static $instance = null;
-    private $workers = []; 
+    private $workers = [];
     private $num = 1;
     private $routine = null;
     private $running = true;
-                                                                                                         
-    private function __construct() {
-    }   
 
-    public static function getInstance() {
+    private function __construct()
+    {
+        $running = true;
+        pcntl_signal(SIGTERM, function (&$running) {
+            $running = false;
+            Util::log("shutdown");
+        }, false);
+        $this->running = $running;
+    }
+
+    public static function getInstance()
+    {
         if (!self::$instance) {
             self::$instance = new self();
-        }   
+        }
 
         return self::$instance;
-    }   
+    }
 
-    public function start($n, $runtine) {
-        $this->num = $n; 
+    public function start($n, $runtine)
+    {
+        $this->num = $n;
 
         if (is_callable($runtine)) {
-            $this->routine = function() use($runtine) {
-                                                                                                         
+            $this->routine = function () use ($runtine) {
+
                 // 子进程则执行
                 if (0 === $this->fork()) {
                     $runtine();
-                }   
-            };  
+                    exit(0);
+                }
+            };
         } elseif (is_string($runtine)) {
-            $this->routine = function() use ($runtine) {
+            $this->routine = function () use ($runtine) {
                 if (0 === $this->fork()) {
                     if (false === pcntl_exec($runtine, $args, $envs)) {
                         exit();
-                    }   
-                }   
-            };  
-        }   
+                    }
+                }
+            };
+        }
 
         $this->execute();
-    }   
+    }
 
-    public function stop($pid) {
+    public function stop($pid)
+    {
         posix_kill($pid, SIGTERM);
 
         while (posix_kill($pid, 0)) {
@@ -59,9 +71,10 @@ class Pool {
         echo "done\n";
     }
 
-    public function fork() {
+    public function fork()
+    {
         $pid = pcntl_fork();
-        $this->log($pid);
+
         if ($pid > 0) {
             $this->workers[$pid] = date('Y-m-d H:i:s');
         }
@@ -69,48 +82,60 @@ class Pool {
         return $pid;
     }
 
-    public function run() {
+    public function run()
+    {
         for ($i = 0; $i < $this->num; $i++) {
             call_user_func($this->routine);
         }
     }
 
-    public function execute() {
+    public function execute()
+    {
         $running = $this->running;
-        pcntl_signal(SIGUSR1, function() use (&$running){
-            $running = false;
+        $stop = false;
+        pcntl_signal(SIGUSR1, function () use (&$stop) {
+            $stop = true;
         });
+
 
         $this->run();
 
         while ($running) {
-            pcntl_signal_dispatch();
+            if ($stop) {
+                pcntl_signal_dispatch();
+                $this->kill(SIGTERM);
+                $stop = false;
+            }
+
             $this->wait();
+
             usleep(40000);
         }
     }
 
-    public function kill($signal) {
-        print_r($this->workers);
+    public function kill($signal)
+    {
         foreach ($this->workers as $k => $v) {
             posix_kill($k, $signal);
         }
     }
 
-    public function wait() {
+    public function wait()
+    {
         // 等待子进程返回的状态
         // $pid 发生错误时返回-1
         foreach ($this->workers as $k => $v) {
             $pid = pcntl_waitpid($k, $status, WNOHANG);
 
             if ($pid > 0) {
-                printf($pid ."\n");
+                printf($pid . "\n");
                 unset($this->workers[$pid]);
             }
         }
     }
 
-    public function clean() {
+    public function clean()
+    {
         // SIG_BLOCK = 0
         foreach ($this->workers as $k => $v) {
             if (!posix_kill($k, 0)) {
@@ -119,13 +144,8 @@ class Pool {
         }
     }
 
-    public function getWorkersNum() {
+    public function getWorkersNum()
+    {
         return count($this->workers);
-    }
-
-    public function log($msg) {
-        printf($msg . "\n");
-        printf("----------------------------------------------------\n");
-        printf("\n\n");
     }
 }
