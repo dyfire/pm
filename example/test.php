3e3fac92a6c8e78aab887dadc0d5cf564f26b322
dyfire <dyfire@gmail.com>
2017-03-01 16:21:38 +0800

    Update test.php

--- example/test.php
+++ example/test.php
@@ -4,15 +4,31 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 use Pm\Pms;
 
-$params = getopt('', ['start']);
+$params = getopt('', ['start::']);
+$status = isset($params['start']) ? $params['start'] : '';
+$pms = new Pms('app.pid', true);
 
-$pms = new Pms('app.pid', false);
-if (!isset($params['start'])) {
-    printf("stop\n");
-    $pms->stop();
-} else {
-    $pms->start(2, function () use ($pms) {
-        printf("run\n");
-        sleep(20);
-    });
+switch ($status) {
+    case 'start':
+        $pms->start(1, function () use ($pms) {
+            printf("run\n");
+            sleep(600);
+        });
+        break;
+    case 'stop':
+        printf("stop\n");
+        $pms->stop();
+        break;
+    case 'restart':
+        $pms->restart();
+        break;
+    default:
+        die(<<<USAGE
+usage:
+    start:\tpm --start --app={app/command} -n {n} [--pid={pid} [--pidfile={pidfile}]] [--wait-bootstrap={senconds}]
+    stop:\tpm --stop [--pid={pid} [--pidfile={pidfile}]]
+    reload:\tpm --reload [--pid={pid} [--pidfile={pidfile}]]
+
+USAGE
+        );
 }
